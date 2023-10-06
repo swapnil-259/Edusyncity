968827436732fcdaad6c9d65a5c46797bba56fde
Nikhil Singh <nikhil.2226csit1110@kiet.edu>
2023-10-06 18:40:02 +0530

    database added

--- EduSyncity/settings.py
+++ EduSyncity/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'EduSyncity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EduSyncity',
+        'USER': 'nikhil',
+        'PASSWORD':'Nik212107@',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
