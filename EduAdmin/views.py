377ac999a4e7e2686138f2d7f747844ec9103f80
swapnil-259 <swapnilagrawal259@gmail.com>
2023-10-07 16:39:22 +0530

    changes in views.py

--- EduAdmin/views.py
+++ EduAdmin/views.py
@@ -54,32 +54,26 @@ def login_user(request):
     if request.method == 'POST':
 
         load=json.loads(request.body)
-        email = load.get('email')
+        username = load.get('username')
         password = load.get('password')
-        user=authenticate(email=email,password=password)
+        user=authenticate(username=username,password=password)
         
-        if email is None or password is None:
+        if username is None or password is None:
             return JsonResponse({'message': 'Missing any Key.'}, status=400)
         
-        if not email or not password:
+        if not username or not password:
             return JsonResponse({'message': 'Missing Required field.'}, status=400)
         
         if user is not None:
             login(request,user)
-
-            role =Roles.objects.get(user=request.user.id)
-            if not role:
-                return JsonResponse({'message':'Yo Not Have Any role'})
-            else:
-                return JsonResponse({'message':'You Are logged in','role':role.name})
-            
+            user_exist = UserRole.objects.filter(user_id = request.user.id).values('role','role__rolename')
+            user_data = list(user_exist)
+            return JsonResponse(user_data, safe=False)
         else:
             return JsonResponse({'message':'Incorrect Username Or password'},status=401)
         
     else:
         return JsonResponse({'message':'Invalid Request Method'},status=400)
-        
-
 
 def logout_user(request):   
     
