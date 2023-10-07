88a16423bec6dc7abc0d586080490ea13da76ea8
swapnil-259 <swapnilagrawal259@gmail.com>
2023-10-07 16:21:51 +0530

    add register_faculty in views.py

--- EduAdmin/urls.py
+++ EduAdmin/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from EduAdmin import views
 
 urlpatterns = [
+      path('register_faculty/', views.register_faculty, name='register_faculty'),
 
 ]
   
--- EduAdmin/views.py
+++ EduAdmin/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
-from .models import User, UserRole
+from django.contrib.auth import authenticate, login, logout
+from .models import User, UserRole, Roles, Faculty
+from django.http import JsonResponse
+import json
 
 
-def RegisterFaculty(request):
+def register_faculty(request):
     if request.method == 'POST':
+      first_name = request.POST.get('firstname')
+      last_name = request.POST.get('lastname')
+      user_name =  request.POST.get('username')
+      email =  request.POST.get('email')
+      gender =  request.POST.get('gender')
+      phone =  request.POST.get('phone')
+      age =  request.POST.get('age')
+      address =  request.POST.get('address')
+      department =  request.POST.get('department')
+      qualification = request.POST.get('qualification')
+      subject =  request.POST.get('subject')
+      profile_pic = request.FILES.get('profilepic')
+      password =  request.POST.get('password')
+      confirm_pass =  request.POST.get('confirmpassword')
+      if password != confirm_pass:
+            return JsonResponse({'message': 'Password and confirmPassword do not match'}, status=400)
+      if user_name or first_name or last_name or email or gender or phone or age or address or password or confirm_pass== None:
+            return JsonResponse({'message':'all details is mandatory'}, status = 400)
+        
+      user=  User.objects.create_user(
+               username = user_name,
+               password = password,
+               first_name=first_name,
+               last_name=last_name,
+               email=email
+             ) 
+      Faculty.objects.create(
+          user_id = user.id,
+          department_id = department,
+          subject_id = subject,
+          qualification = qualification,
+          address = address,
+          profile_picture = profile_pic   
+      )
+      roles, created = Roles.objects.get_or_create(rolename= 'Faculty')
+      UserRole.objects.create(
+          user_id = user.id,
+          role_id = roles.id,
+          department = department
+      )
+        
         
