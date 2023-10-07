3e0ec0fe82ffca6ac059cdbd534aaba941bf1661
swapnil-259 <swapnilagrawal259@gmail.com>
2023-10-07 14:55:13 +0530

    changes in model.py

--- EduAdmin/models.py
+++ EduAdmin/models.py
@@ -7,7 +7,10 @@ class User(AbstractUser):
 class Courses(models.Model):
     courses_name = models.CharField(null = True, max_length=100)
     course = models.ForeignKey("Courses", on_delete=models.CASCADE)
-
+class Subjects(models.Model):
+    subject_name = models.CharField(max_length=100, null=True)
+    subject_code = models.CharField(max_length=50, null=True)
+    
     
     
     
