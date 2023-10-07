a34141fce74c99daf206dd928bbb88ecc56b8155
Nikhil Singh <nikhil.2226csit1110@kiet.edu>
2023-10-07 14:46:57 +0530

    new models

--- EduAdmin/models.py
+++ EduAdmin/models.py
@@ -4,7 +4,30 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Courses(models.Model):
-    courses_name = models.CharField(null = True, max_length=100)
-    course = models.ForeignKey("Courses", on_delete=models.CASCADE)
+class Basemodel(models.Model):
+    created_time=models.DateTimeField(auto_now=True)
+    deleted_status=models.BooleanField(default=False)
+    deleted_time=models.DateTimeField(null=True)
+
+
+class DepartmentUnderCourses(Basemodel):
+    name = models.CharField(null = True, max_length=100)
+    course = models.ForeignKey("DepartmentUnderCourses", on_delete=models.CASCADE)
     
+
+
+class Roles(Basemodel):
+    role_name=models.CharField(max_length=50,null=True)
+
+class UserRole(Basemodel):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    department=models.ForeignKey(DepartmentUnderCourses,on_delete=models.CASCADE,null=True)
+    role=models.ForeignKey(Roles,on_delete=models.CASCADE,null=True)
+   
+
+
+class Faculty(Basemodel):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    department=models.ForeignKey(DepartmentUnderCourses,on_delete=models)
+    subject=models.ForeignKey(Subjects,on_delete=models.CASCADE,null=True)
+    qualification=models.CharField(null=True,max_length=100)
