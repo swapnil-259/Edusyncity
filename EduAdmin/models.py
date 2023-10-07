8e188c3ce9318c0f4f4fe437c7a1d23e51e2f19a
Nikhil Singh <nikhil.2226csit1110@kiet.edu>
2023-10-07 14:56:05 +0530

    changes

--- EduAdmin/models.py
+++ EduAdmin/models.py
@@ -4,23 +4,28 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Courses(models.Model):
+class BaseModel(models.Model):
+    created_time=models.DateTimeField(auto_now_add=True)
+    deleted_status = models.BooleanField(default=0)
+    deleted_time = models.DateTimeField(null=True)
+
+class DepartmentUnderCourses(models.Model):
     courses_name = models.CharField(null = True, max_length=100)
     course = models.ForeignKey("Courses", on_delete=models.CASCADE)
     
 
 
-class Roles(Basemodel):
+class Roles(BaseModel):
     role_name=models.CharField(max_length=50,null=True)
 
-class UserRole(Basemodel):
+class UserRole(BaseModel):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     department=models.ForeignKey(DepartmentUnderCourses,on_delete=models.CASCADE,null=True)
     role=models.ForeignKey(Roles,on_delete=models.CASCADE,null=True)
    
 
 
-class Faculty(Basemodel):
+class Faculty(BaseModel):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     department=models.ForeignKey(DepartmentUnderCourses,on_delete=models)
     subject=models.ForeignKey(Subjects,on_delete=models.CASCADE,null=True)
