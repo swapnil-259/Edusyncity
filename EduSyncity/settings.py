28059f73a747aefc8f3b208e6ef663f8de4ed813
swapnil-259 <swapnilagrawal259@gmail.com>
2023-10-07 10:32:05 +0530

    change in files

--- EduSyncity/settings.py
+++ EduSyncity/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Edusyncity',
-        'USER': 'swapnil',
+        'USER': 'harry',
         'PASSWORD':'Kiet@123',
-        'HOST':'0.0.0.0',
+        'HOST':'%',
         'PORT':'3306',
     }
 }
