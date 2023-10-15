018dc7d536d928f53567dcd239720ba66af473e3
Nikhil Singh <nikhil.2226csit1110@kiet.edu>
2023-10-15 21:45:28 +0530

    sending data

--- EduAdmin/views.py
+++ EduAdmin/views.py
@@ -262,9 +262,18 @@ def sidebar(request):
         if request.user.is_authenticated:
              check_admin = UserRole.objects.filter(role_id = '1').first()
              if check_admin:
-                 master_configuration = Dropdown.objects.filter(pannel=1).values('pk','name','child','state','icon','type')
-                 master_configuration_list = list(master_configuration)
-                 return JsonResponse(master_configuration_list, safe=False)
+                 pannel_data=Dropdown.objects.filter(pannel=1).values('pk')
+                #  print(pannel_data)
+                 master_configuration = list(Dropdown.objects.filter(pannel=1).values('pk','name','child','state','icon','type'))
+                 
+                 for pannel in pannel_data:
+                    
+                    pk=list(Dropdown.objects.filter(relation=pannel.get('pk')).values('name','child','state','icon','type'))
+                    master_configuration.append(pk)
+
+                           
+                #  print(master_configuration)   
+                 return JsonResponse(master_configuration, safe=False)
              else:
                  return JsonResponse({'message':'user is not admin'})
         else:
