00e72dd2c18468b562e0118dcbb09db0215fb396
swapnil-259 <swapnilagrawal259@gmail.com>
2023-10-15 11:22:08 +0530

    change in login views

--- EduAdmin/urls.py
+++ EduAdmin/urls.py
@@ -3,7 +3,7 @@ from EduAdmin import views
 
 urlpatterns = [
       path('register_faculty/', views.register_faculty, name='register_faculty'),
-     path('login/', views.login_user),
+     path('login/', views.login_user, name = 'login'),
      path('logout/', views.logout_user),
 
 ]
