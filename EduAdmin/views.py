973f40b69d2baf4de6aeb05f2e5d9a5d48b7fe0a
swapnil-259 <swapnilagrawal259@gmail.com>
2023-10-16 21:16:22 +0530

    add changes

--- EduAdmin/views.py
+++ EduAdmin/views.py
@@ -391,6 +391,45 @@ def register_student(request):
             return JsonResponse({'message':'Registration Succesfull'},status=201)
     else:
         return JsonResponse({'message':'Invalid Request Method'},status=405)
+def add_dropdown(request):
+    if request.method == 'POST':
+        load = json.loads(request.body)
+        if request.user.is_authenticated:
+            check_admin = UserRole.objects.filter(role_id = '1').first()
+            if check_admin:
+                id = load.get('id')
+                name = load.get('name')
+                child = load.get('child')
+                dropdown_id_exist=Dropdown.objects.filter(id = id, child__gte=0).first()
+                if dropdown_id_exist is not None:
+                    dropdown, created=Dropdown.objects.get_or_create(
+                        name= name,
+                        child = child,
+                        added_by=check_admin.user_id,
+                        relation_id = dropdown_id_exist.id,
+                        type=type
+                    )
+                    if created:
+                        childs=dropdown_id_exist.child
+                        childs=int(childs)-1
+                        dropdown_id_exist.child= childs
+                        dropdown_id_exist.save()
+                        return JsonResponse({'message':'dropdown created successfully'})
+                    else:
+                        return JsonResponse({'message':'dropdown already exist'})
+                else:
+                    return JsonResponse({'message':'not more child added'})
+                
+            else:
+                return JsonResponse({'message':'user is not admin'})
+        else:
+            return JsonResponse({'message':'user is not authenticated'})
+        
+                
+                
+                        
+            
+            
 
         
         
