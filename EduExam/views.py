62b716b1f6cf57d365e97fd2ee96c9999f503543
Nikhil Singh <nikhil.2226csit1110@kiet.edu>
2023-10-20 19:37:33 +0530

    get mapping

--- EduExam/views.py
+++ EduExam/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 import json
 from django.http import JsonResponse
-from .models import PaperDetails,Questions
+from .models import PaperDetails,Questions,ExamMapping
 from EduAdmin.models import UserRole,Dropdown,Mapping,Subjects,User
+
+
 
 def paper_details(request):
     if request.method=='POST':
@@ -64,10 +66,9 @@ def exam_type(request):
 def exam_info(request):
     if request.method == 'GET':
         id=request.GET.get('id')
-        time=Dropdown.objects.filter(deleted_status=False,relation='80').values('name')
-        marks=Dropdown.objects.filter(deleted_status=False,relation=id).values('name')
-        data=[list(time),list(marks)]
-        if time and marks:
+        data=ExamMapping.objects.filter(deleted_status=False,exam=id).values('marks__name','duration__name')
+        
+        if data:
             return JsonResponse(data,safe=False)
         else:
             return JsonResponse({'message':'No Content'},status=204)
